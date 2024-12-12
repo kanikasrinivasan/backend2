2f4251327e9649c74946612cb817807770033aa0
kanikasrinivasan <kanikasrinivasan4@gmail.com>
2024-12-12 23:24:18 +0530

    done

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.get('/', (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
